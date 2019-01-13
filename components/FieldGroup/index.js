61692c98645c769fac2f890901091aebc48df626
Wojciech Grzebieniowski <grzebieniowski@gmail.com>
2019-01-13 15:50:25 +0100

    Add marginBottom support

--- components/FieldGroup/index.js
+++ components/FieldGroup/index.js
@@ -4,11 +4,12 @@ import styles from "./styles";
 
 class FieldGroup extends PureComponent {
   render() {
-    const count = React.Children.count(this.props.children);
+    const { children, marginBottom = 1 } = this.props;
+    const count = React.Children.count(children);
 
     return (
-      <View style={styles.container}>
-        {React.Children.map(this.props.children, (input, i) => (
+      <View style={{ ...styles.container, marginBottom: 16 * marginBottom }}>
+        {React.Children.map(children, (input, i) => (
           <React.Fragment key={`input-${i}`}>
             <View style={styles.item}>{input}</View>
             {count > 1 && i < count - 1 && <View style={styles.separator} />}
--- components/Header/index.js
+++ components/Header/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import { View, Text } from "react-native";
 import styles from "./styles";
 
-function Header({ leftButton = null, rightButton = null, title }) {
+function Header({ leftButton = null, rightButton = null, title, marginBottom = 0 }) {
   return (
-    <View style={styles.container}>
+    <View style={{ ...styles.container, marginBottom: marginBottom * 16 }}>
       <View style={styles.leftButton}>{leftButton}</View>
       <Text style={styles.heading}>{title}</Text>
       <View style={styles.rightButton}>{rightButton}</View>
