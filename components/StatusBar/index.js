054fb95c52b56f4f2276016ead1947e141ee6eba
Wojciech Grzebieniowski <grzebieniowski@gmail.com>
2019-01-14 14:05:17 +0100

    Remove height from StatusBar

--- components/StatusBar/index.js
+++ components/StatusBar/index.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { View, StatusBar as NativeStatusBar, Platform } from "react-native";
+import { View, StatusBar as NativeStatusBar } from "react-native";
 
 export default function StatusBar({ backgroundColor, barStyle }) {
-  const height = Platform.OS === "ios" ? 20 : 0;
   return (
-    <View style={{ height, backgroundColor }}>
+    <View style={{ backgroundColor }}>
       <NativeStatusBar backgroundColor={backgroundColor} barStyle={barStyle} />
     </View>
   );
