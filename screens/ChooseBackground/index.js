f6508650f3edde35bfe1ff9ee51c8eb3bbb247b9
Wojciech Grzebieniowski <grzebieniowski@gmail.com>
2019-01-15 19:30:13 +0100

    Fetch multiple pages

--- screens/ChooseBackground/index.js
+++ screens/ChooseBackground/index.js
@@ -8,17 +8,7 @@ import TextButton from "../../components/TextButton";
 import Margin from "../../components/Margin";
 import SearchBar from "../../components/SearchBar";
 import { TEXT_COLOR } from "../../theme";
-
-const promiseSerial = funcs =>
-  funcs.reduce(
-    (promise, func) => promise.then(result => func().then(Array.prototype.concat.bind(result))),
-    Promise.resolve([])
-  );
-
-const fetchPicture = () =>
-  fetch("https://source.unsplash.com/random").then(response => ({
-    uri: response.url,
-  }));
+import { searchPhotos } from "../../utils/unsplash";
 
 class ChooseBackground extends PureComponent {
   static navigationOptions = ({ navigation }) => {
@@ -27,41 +17,87 @@ class ChooseBackground extends PureComponent {
       headerLeft: <TextButton title="Back" icon="arrow-back" onPress={() => navigation.goBack()} />,
     };
   };
+
   state = {
+    loading: false,
+    page: 1,
     query: "",
     results: [],
-    loading: false,
-    galleryWidth: 0,
   };
+  galleryWidth = 0;
+  onEndReachedCalledDuringMomentum = false;
+
   componentDidMount() {
-    this.searchForImages();
+    this.fetchPictures();
   }
-  searchForImages = query => {
-    this.setState({ loading: true });
-    promiseSerial(
-      Array(10)
-        .fill()
-        .map(() => fetchPicture)
-    ).then(results => {
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.query && this.state.query === "") {
+      this.setState({ results: [] }); // clear the results
+    }
+  }
+
+  _fetchPictures = async (query, page) => {
+    try {
+      console.log(`Searching for ${query}`);
+      this.setState({ loading: true });
+      const photos = await searchPhotos(query, page);
+
+      return photos.map(photo => ({
+        author: photo.user.name,
+        id: photo.id,
+        uri: photo.urls.small,
+      }));
+    } catch (err) {
+      // @todo - show error
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  fetchPictures = () => {
+    this._fetchPictures(this.state.query, this.state.page).then(newResults => {
       this.setState({
-        results,
-        loading: false,
+        results: newResults,
       });
     });
   };
-  throttledSearchForImages = debounce(this.searchForImages, 800);
+
+  fetchNextPage = () => {
+    this._fetchPictures(this.state.query, this.state.page + 1).then(newResults => {
+      this.setState(state => ({
+        results: state.results.concat(newResults),
+        page: state.page + 1,
+      }));
+    });
+  };
+
+  fetchPicturesThrottled = debounce(this.fetchPictures, 800);
+
   _setQuery = query => {
     this.setState({ query });
-    this.throttledSearchForImages(query);
+    if (query.length) this.fetchPicturesThrottled();
   };
+
   _onGalleryLayout = event => {
     const { width } = event.nativeEvent.layout;
-    this.setState({ galleryWidth: width - 32 });
+    this.galleryWidth = width - 32;
   };
-  _galleryKeyExtractor = (item, index) => `gallery-${index}`;
+
+  _galleryKeyExtractor = (_, index) => `gallery-${index}`;
+
+  _resetOnEndReachedCalledDuringMomentum = () => (this.onEndReachedCalledDuringMomentum = false);
+
+  _onEndReached = () => {
+    if (!this.onEndReachedCalledDuringMomentum) {
+      this.fetchNextPage(this.state.query, this.state.page);
+      this.onEndReachedCalledDuringMomentum = true;
+    }
+  };
+
   render() {
     const chunkSize = 2;
-    const imageSize = (this.state.galleryWidth - (chunkSize - 1) * 16) / chunkSize;
+    const imageSize = (this.galleryWidth - (chunkSize - 1) * 16) / chunkSize;
     return (
       <View style={styles.screen} onLayout={this._onGalleryLayout}>
         <View style={styles.padding}>
@@ -71,45 +107,49 @@ class ChooseBackground extends PureComponent {
           <SearchBar value={this.state.query} onChangeText={this._setQuery} />
         </View>
         <Margin top={2} style={{ flex: 1 }}>
-          {this.state.loading && (
-            <View style={styles.loaderWrapper}>
-              <ActivityIndicator size="large" color={TEXT_COLOR} />
-            </View>
-          )}
-          {!this.state.loading && (
-            <FlatList
-              keyExtractor={this._galleryKeyExtractor}
-              data={chunk(this.state.results, chunkSize)}
-              style={styles.padding}
-              onEndReached={console.log.bind(console)}
-              renderItem={({ item }, i) => (
-                <View
-                  style={{
-                    flexDirection: "row",
-                    flex: 1,
-                    marginBottom: 16,
-                    alignContent: "center",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  {item.map((image, i) => (
-                    <ImageBackground
-                      key={`image-${i}`}
-                      imageStyle={{ borderRadius: 5 }}
-                      style={{
-                        borerRadius: 5,
-                        width: imageSize,
-                        height: imageSize,
-                      }}
-                      source={{
-                        uri: image.uri,
-                      }}
-                    />
-                  ))}
-                </View>
-              )}
-            />
-          )}
+          <FlatList
+            keyExtractor={this._galleryKeyExtractor}
+            data={chunk(this.state.results, chunkSize)}
+            style={styles.padding}
+            ListFooterComponent={() => {
+              return (
+                this.state.loading && (
+                  <View style={styles.loaderWrapper}>
+                    <ActivityIndicator size="large" color={TEXT_COLOR} />
+                  </View>
+                )
+              );
+            }}
+            onMomentumScrollBegin={this._resetOnEndReachedCalledDuringMomentum}
+            onEndReachedThreshold={0.5}
+            onEndReached={this._onEndReached}
+            renderItem={({ item }, i) => (
+              <View
+                style={{
+                  flexDirection: "row",
+                  flex: 1,
+                  marginBottom: 16,
+                  alignContent: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                {item.map((image, i) => (
+                  <ImageBackground
+                    key={`image-${i}`}
+                    imageStyle={{ borderRadius: 5 }}
+                    style={{
+                      borerRadius: 5,
+                      width: imageSize,
+                      height: imageSize,
+                    }}
+                    source={{
+                      uri: image.uri,
+                    }}
+                  />
+                ))}
+              </View>
+            )}
+          />
         </Margin>
       </View>
     );
