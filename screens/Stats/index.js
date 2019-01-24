4899c0786d250bbf1ea1354f7762f7cc15123641
Wojciech Grzebieniowski <grzebieniowski@gmail.com>
2019-01-24 20:03:52 +0100

    Add stats view

---
+++ screens/Stats/index.js
@@ -0,0 +1,137 @@
+import format from "date-fns/format";
+import compose from "lodash/fp/compose";
+import groupBy from "lodash/fp/groupBy";
+import map from "lodash/fp/map";
+import mapValues from "lodash/fp/mapValues";
+import toPairs from "lodash/fp/toPairs";
+import noop from "lodash/noop";
+import PropTypes from "prop-types";
+import React, { PureComponent } from "react";
+import { Dimensions, ImageBackground, Text, View } from "react-native";
+import { LineChart } from "react-native-chart-kit";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import FieldGroup from "../../components/FieldGroup";
+import Select from "../../components/Select";
+import TextButton from "../../components/TextButton";
+import TextInput from "../../components/TextInput";
+import {
+  removeCounter,
+  RESET_DAILY,
+  RESET_MONTHLY,
+  RESET_NEVER,
+  RESET_WEEKLY,
+  RESET_YEARLY,
+  updateCounter,
+} from "../../reducers/counters";
+import { BRAND_PRIMARY, BACKGROUND_COLOR } from "../../theme";
+import styles from "./styles";
+
+const ResetLabels = {
+  [RESET_NEVER]: "Never",
+  [RESET_DAILY]: "Daily",
+  [RESET_WEEKLY]: "Weekly",
+  [RESET_MONTHLY]: "Monthly",
+  [RESET_YEARLY]: "Yearly",
+};
+
+class Stats extends PureComponent {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerTitle: "Statistics",
+      headerLeft: (
+        <TextButton
+          onPress={() => navigation.navigate("Main")}
+          icon="arrow-back"
+          title="Back"
+          color={BRAND_PRIMARY}
+        />
+      ),
+    };
+  };
+
+  static propTypes = {
+    counter: PropTypes.shape({
+      background: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        uri: PropTypes.string.isRequired,
+      }),
+      id: PropTypes.string.isRequired,
+      reset: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+      navigate: PropTypes.func,
+      setParams: PropTypes.func,
+    }),
+    transitions: PropTypes.arrayOf(
+      PropTypes.shape({
+        date: PropTypes.number.isRequired,
+        id: PropTypes.string.isRequired,
+        transition: PropTypes.number.isRequired,
+        counterId: PropTypes.string.isRequired,
+      })
+    ),
+  };
+
+  static defaultProps = {
+    counter: {},
+    navigation: {
+      getParam: noop,
+      navigate: noop,
+      setParams: noop,
+    },
+    transitions: [],
+  };
+
+  transformData = compose(
+    map(([x, y]) => ({ x, y })),
+    toPairs,
+    mapValues(values => values.reduce((acc, transition) => acc + transition.transition, 0)),
+    groupBy(({ date }) => format(date, "YYYY-MM-DD"))
+  );
+
+  render() {
+    const chartData = this.transformData(this.props.transitions);
+    return (
+      <View style={styles.container}>
+        <LineChart
+          data={{
+            labels: chartData.map(({ x }) => x),
+            datasets: [
+              {
+                data: chartData.map(({ y }) => y),
+              },
+            ],
+          }}
+          width={Dimensions.get("window").width} // from react-native
+          height={220}
+          chartConfig={{
+            backgroundColor: BACKGROUND_COLOR,
+            backgroundGradientFrom: BACKGROUND_COLOR,
+            backgroundGradientTo: BACKGROUND_COLOR,
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: BRAND_PRIMARY,
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+export default connect(
+  ({ counters, transitions }, { navigation }) => {
+    const counterId = navigation.getParam("id", null);
+    return {
+      counter: counters.find(counter => counter.id === counterId),
+      transitions: transitions.filter(transition => transition.counterId === counterId),
+    };
+  },
+  dispatch => bindActionCreators({ removeCounter, updateCounter }, dispatch)
+)(Stats);
---
+++ screens/Stats/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import { BACKGROUND_COLOR } from "../../theme";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: BACKGROUND_COLOR,
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default styles;
